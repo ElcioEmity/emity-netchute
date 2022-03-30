8e5cf7e38f2ddae345a9bd6af3b1ecff9476f92c
Elcio <86811239+ElcioEmity@users.noreply.github.com>
2022-03-30 20:51:28 -0300

    subindo arquivos - primeiro commit

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bdee643b9e5369', '71f47ba0', 'heroku_0da5f18de0d5403') or die('Erro ao conectar ao banco de dados');
